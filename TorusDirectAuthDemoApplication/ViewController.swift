3a081136acf86e5e19f92b78c4653155d62d7429
rathishubham7 <shubhamrathi@icloud.com>
2021-04-06 16:04:33 +0530

    updated example with new SFURLcontroller

--- TorusDirectAuthDemoApplication/ViewController.swift
+++ TorusDirectAuthDemoApplication/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import TorusSwiftDirectSDK
+import PromiseKit
 import SafariServices
 
 class ViewController: UIViewController {
@@ -34,30 +35,44 @@ class ViewController: UIViewController {
 
         let tdsdk = TorusSwiftDirectSDK(aggregateVerifierType: .singleLogin, aggregateVerifierName: "torus-auth0-github-lrc", subVerifierDetails: [sub], loglevel: .info)
         
-        tdsdk.triggerLogin(controller: self, browserType: .sfsafari, modalPresentationStyle: .popover).done{ data in
-            print("private key rebuild", data)
-
-            let alert = UIAlertController(title: "Public address", message: data["publicAddress"] as? String, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-                switch action.style{
-                    case .default:
-                        print("default")
-
-                    case .cancel:
-                        print("cancel")
-
-                    case .destructive:
-                        print("destructive")
-
-
-                    @unknown default:
-                        fatalError()
-                }}))
-            self.present(alert, animated: true, completion: nil)
-
-        }.catch{ err in
-            print(err)
+        // listening to notifications after redirect from browser
+        // callback pattern is used because there are many ways to get the idToken from a service provider
+        // for ex. Email redirect, browser, different app, same app but different component.
+        
+        // You can implement the same without observerCallback <> Notification pattern
+        // Pass the idToken from AppDeletegate directly to sdk.getTorusKey()
+        tdsdk.observeCallback{ url in
+            let responseParameters = tdsdk.parseURL(url: url)
+            sub.getUserInfo(responseParameters: responseParameters).then{ newData -> Promise<[String: Any]> in
+                var data = newData
+                let verifierId = data["verifierId"] as! String
+                let idToken = data["tokenForKeys"] as! String
+                data.removeValue(forKey: "tokenForKeys")
+                data.removeValue(forKey: "verifierId")
+                
+                return tdsdk.getTorusKey(verifier: "torus-auth0-github-lrc", verifierId: verifierId, idToken: idToken, userData: data)
+            }.done{data in
+                print(data)
+                // Do other tasks here
+            }.catch{err in
+                print(err)
+                // In case of an error
+            }
         }
+        
+        // Get the loginURL
+        let loginURL = sub.getLoginURL()
+        let handler = SFURLHandler(viewController: self)
+        // Attach events here
+        handler.presentCompletion = {
+            print("Safari presented")
+        }
+        handler.dismissCompletion = {
+            // Attach handler for dismissal
+            print("Safari dismissed")
+        }
+        // trigger login using SFSafari browser
+        handler.handle(URL(string: loginURL)!, modalPresentationStyle: modalPresentationStyle)
     }
     
     
